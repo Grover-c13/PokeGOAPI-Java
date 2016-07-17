4b3e0795f2398769985e6c02131ef744a8bea819
Jasper Abbink <git@abb.ink>
2016-07-17 12:36:50 +0200

    Add login via Pokemon Trainers Club

--- src/auth/Login.java
+++ src/auth/Login.java
@@ -3,11 +3,10 @@ package auth;
 import exceptions.LoginFailedException;
 import main.Pokemon;
 
-public abstract class Login 
-{
-	
-	public abstract Pokemon.RequestEnvelop.AuthInfo login(String username, String password) throws LoginFailedException;
-	
-	public abstract  Pokemon.RequestEnvelop.AuthInfo login(String token) throws LoginFailedException;
-	
+public abstract class Login {
+
+  public abstract Pokemon.RequestEnvelop.AuthInfo login(String username, String password) throws LoginFailedException;
+
+  public abstract Pokemon.RequestEnvelop.AuthInfo login(String token) throws LoginFailedException;
+
 }
---
+++ src/auth/PTCAuthJson.java
@@ -0,0 +1,22 @@
+package auth;
+
+public class PTCAuthJson {
+  private String lt;
+  private String execution;
+
+  public String getLt() {
+    return lt;
+  }
+
+  public void setLt(String lt) {
+    this.lt = lt;
+  }
+
+  public String getExecution() {
+    return execution;
+  }
+
+  public void setExecution(String execution) {
+    this.execution = execution;
+  }
+}
