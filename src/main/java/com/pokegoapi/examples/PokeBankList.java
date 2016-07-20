577280c07760004fb6bfa71593c97727ec932df0
Mike Tsai <mtsai@apple.com>
2016-07-20 16:37:59 -0700

    Removed the POGOProtos lib. Removed the userReader.

--- src/main/java/com/pokegoapi/examples/PokeBankList.java
+++ src/main/java/com/pokegoapi/examples/PokeBankList.java
@@ -1,22 +1,15 @@
 package com.pokegoapi.examples;
 
-import POGOProtos.Enums.PokemonIdOuterClass;
 import POGOProtos.Networking.Envelopes.RequestEnvelopeOuterClass;
-import POGOProtos.Networking.Responses.ReleasePokemonResponseOuterClass;
 import com.pokegoapi.api.PokemonGo;
 import com.pokegoapi.api.inventory.PokeBank;
-import com.pokegoapi.api.pokemon.Pokemon;
 import com.pokegoapi.auth.GoogleLogin;
-import com.pokegoapi.auth.UserReader;
-import com.pokegoapi.exceptions.LoginFailedException;
 import com.pokegoapi.exceptions.RemoteServerException;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -24,15 +17,9 @@ public class PokeBankList {
     public static void main(String[] args) {
         OkHttpClient http = new OkHttpClient.Builder().connectTimeout(60, TimeUnit.SECONDS).build();
         RequestEnvelopeOuterClass.RequestEnvelope.AuthInfo auth = null;
-        UserReader userReader = null;
 
         try {
-            userReader = UserReader.getUserReader(UserReader.GOOGLE_USERS);
-            String[] user = userReader.getSingle();
-            if (user == null)
-                throw new IOException("Cannot get token from google users list");
-
-            String token = user[0];
+            String token = "";
             auth = new GoogleLogin(http).login(token); // currently uses oauth flow so no user or pass needed
             PokemonGo go = new PokemonGo(auth, http);
 
@@ -42,7 +29,7 @@ public class PokeBankList {
                 throw new RemoteServerException("pokeBank is null");
 
             log.debug(pokeBank.toString());
-        } catch (IOException | RemoteServerException e) {
+        } catch (RemoteServerException e) {
             log.error(ExceptionUtils.getStackTrace(e));
         }
     }
