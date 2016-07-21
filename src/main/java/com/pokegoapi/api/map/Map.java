55be76c7155644fb4548ca4f0c8bfd84c44a3653
Matthew J. Mjelde <mjmjelde@ne.tamu.edu>
2016-07-21 11:57:59 -0500

    Fixed the license

--- src/main/java/com/pokegoapi/api/map/Map.java
+++ src/main/java/com/pokegoapi/api/map/Map.java
@@ -1,3 +1,17 @@
+/*
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.pokegoapi.api.map;
 
 import POGOProtos.Map.Fort.FortDataOuterClass;
@@ -32,9 +46,6 @@ import java.util.List;
 
 import static com.pokegoapi.google.common.geometry.S2CellId.MAX_LEVEL;
 
-/**
- * Created by mjmjelde on 7/21/2016.
- */
 public class Map {
 
 	private final PokemonGo api;
@@ -259,7 +270,7 @@ public class Map {
 			result = cachedMapObjects;
 			lastMapUpdate = System.currentTimeMillis();
 		}
-		
+
 		return result;
 	}
 
