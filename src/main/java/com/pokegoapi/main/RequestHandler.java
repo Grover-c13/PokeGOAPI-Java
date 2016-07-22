f8b2aacd54bf16c283fe64ef00bfd175430ab644
Jasper Abbink <git@abb.ink>
2016-07-22 13:45:03 +0200

    refresh expired auth tickets

--- src/main/java/com/pokegoapi/main/RequestHandler.java
+++ src/main/java/com/pokegoapi/main/RequestHandler.java
@@ -246,9 +246,12 @@ public class RequestHandler {
 	private void resetBuilder(RequestEnvelopeOuterClass.RequestEnvelope.Builder builder) {
 		builder.setStatusCode(2);
 		builder.setRequestId(8145806132888207460L);
-		if (lastAuth != null && lastAuth.getExpireTimestampMs() > 0) {
+		if (lastAuth != null &&
+				lastAuth.getExpireTimestampMs() > 0 &&
+				lastAuth.getExpireTimestampMs() > System.currentTimeMillis()) {
 			builder.setAuthTicket(lastAuth);
 		} else {
+			Log.d(TAG, "Authenticated with static token");
 			builder.setAuthInfo(auth);
 		}
 		builder.setUnknown12(989);
