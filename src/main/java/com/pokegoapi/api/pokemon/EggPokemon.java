8fe3285fef5abfe4516062129a6d2a7180fcbb0a
Grover-c13 <Grover.c13@gmail.com>
2016-07-23 17:27:44 +0800

    Eggs

---
+++ src/main/java/com/pokegoapi/api/pokemon/EggPokemon.java
@@ -0,0 +1,112 @@
+/*
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.pokegoapi.api.pokemon;
+
+import POGOProtos.Data.PokemonDataOuterClass.PokemonData;
+import POGOProtos.Enums.PokemonFamilyIdOuterClass.PokemonFamilyId;
+import POGOProtos.Enums.PokemonIdOuterClass;
+import POGOProtos.Enums.PokemonMoveOuterClass;
+import POGOProtos.Inventory.Item.ItemIdOuterClass.ItemId;
+import POGOProtos.Networking.Requests.Messages.EvolvePokemonMessageOuterClass.EvolvePokemonMessage;
+import POGOProtos.Networking.Requests.Messages.NicknamePokemonMessageOuterClass.NicknamePokemonMessage;
+import POGOProtos.Networking.Requests.Messages.ReleasePokemonMessageOuterClass.ReleasePokemonMessage;
+import POGOProtos.Networking.Requests.RequestTypeOuterClass.RequestType;
+import POGOProtos.Networking.Responses.EvolvePokemonResponseOuterClass.EvolvePokemonResponse;
+import POGOProtos.Networking.Responses.NicknamePokemonResponseOuterClass.NicknamePokemonResponse;
+import POGOProtos.Networking.Responses.ReleasePokemonResponseOuterClass.ReleasePokemonResponse;
+import POGOProtos.Networking.Responses.ReleasePokemonResponseOuterClass.ReleasePokemonResponse.Result;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.pokegoapi.api.PokemonGo;
+import com.pokegoapi.api.map.pokemon.EvolutionResult;
+import com.pokegoapi.exceptions.LoginFailedException;
+import com.pokegoapi.exceptions.RemoteServerException;
+import com.pokegoapi.main.ServerRequest;
+import com.pokegoapi.util.Log;
+import lombok.Setter;
+
+/**
+ * The egg pokemon.
+ */
+public class EggPokemon  {
+
+	private static final String TAG = EggPokemon.class.getSimpleName();
+	@Setter
+	PokemonGo pgo;
+	private PokemonData proto;
+
+	// API METHODS //
+
+	// DELEGATE METHODS BELOW //
+	public EggPokemon(PokemonData proto) {
+		this.proto = proto;
+	}
+
+	public long getId() {
+		return proto.getId();
+	}
+
+	public boolean getIsEgg() {
+		return proto.getIsEgg();
+	}
+
+	public double getEggKmWalkedTarget() {
+		return proto.getEggKmWalkedTarget();
+	}
+
+	public double getEggKmWalkedStart() {
+		return proto.getEggKmWalkedStart();
+	}
+
+	public long getCapturedCellId() {
+		return proto.getCapturedCellId();
+	}
+
+	public long getCreationTimeMs() {
+		return proto.getCreationTimeMs();
+	}
+
+
+	@Override
+	public int hashCode() {
+		return proto.getPokemonId().hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof EggPokemon) {
+			EggPokemon other = (EggPokemon) obj;
+			return (this.getId() == other.getId());
+		}
+
+		return false;
+	}
+	// TODO: add wrapper objects for encubators and allow to be got.
+}
