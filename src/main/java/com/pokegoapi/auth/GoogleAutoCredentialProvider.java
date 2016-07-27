8eea7476c9bde755fc80224825187ee8d29d70d9
svarzee <svarzee@gmail.com>
2016-07-27 20:55:05 +0200

    Add token refresh

--- src/main/java/com/pokegoapi/auth/GoogleAutoCredentialProvider.java
+++ src/main/java/com/pokegoapi/auth/GoogleAutoCredentialProvider.java
@@ -21,39 +21,32 @@ public class GoogleAutoCredentialProvider extends CredentialProvider {
 	private static String GOOGLE_LOGIN_APP = "com.nianticlabs.pokemongo";
 	private static String GOOGLE_LOGIN_CLIENT_SIG = "321187995bc7cdc2b5fc91b11a96e2baa8602c62";
 
+	private final Gpsoauth gpsoauth;
 	private final String username;
-	private final String password;
+	private TokenInfo tokenInfo;
 
-	private AuthToken authToken;
-	private AuthInfo authInfo;
 
 	/**
 	 * Constructs credential provider using username and password
 	 *
 	 * @param username - google username
 	 * @param password - google password
-	 * @throws LoginFailedException - login failed possibly due to invalid credentials
+	 * @throws LoginFailedException  - login failed possibly due to invalid credentials
 	 * @throws RemoteServerException - some server/network failure
 	 */
-	public GoogleAutoCredentialProvider(String username, String password)
+	public GoogleAutoCredentialProvider(OkHttpClient httpClient, String username, String password)
 			throws LoginFailedException, RemoteServerException {
+		this.gpsoauth = new Gpsoauth(httpClient);
 		this.username = username;
-		this.password = password;
-		login(username, password);
+		this.tokenInfo = login(username, password);
 	}
 
-	private void login(String username, String password) throws RemoteServerException, LoginFailedException {
+	private TokenInfo login(String username, String password) throws RemoteServerException, LoginFailedException {
 		try {
-			OkHttpClient httpClient = new OkHttpClient();
-			Gpsoauth gpsoauth = new Gpsoauth(httpClient);
-			this.authToken = gpsoauth.login(
-					username,
-					password,
-					GOOGLE_LOGIN_ANDROID_ID,
-					GOOGLE_LOGIN_SERVICE,
-					GOOGLE_LOGIN_APP,
-					GOOGLE_LOGIN_CLIENT_SIG
-			);
+			String masterToken = gpsoauth.performMasterLoginForToken(username, password, GOOGLE_LOGIN_ANDROID_ID);
+			AuthToken authToken = gpsoauth.performOAuthForToken(username, masterToken, GOOGLE_LOGIN_ANDROID_ID,
+					GOOGLE_LOGIN_SERVICE, GOOGLE_LOGIN_APP, GOOGLE_LOGIN_CLIENT_SIG);
+			return new TokenInfo(authToken, masterToken);
 		} catch (IOException e) {
 			throw new RemoteServerException(e);
 		} catch (Gpsoauth.TokenRequestFailed e) {
@@ -61,23 +54,46 @@ public class GoogleAutoCredentialProvider extends CredentialProvider {
 		}
 	}
 
+	private TokenInfo refreshToken(String username, String refreshToken) throws RemoteServerException, LoginFailedException {
+		try {
+			AuthToken authToken = gpsoauth.performOAuthForToken(username, refreshToken, GOOGLE_LOGIN_ANDROID_ID,
+					GOOGLE_LOGIN_SERVICE, GOOGLE_LOGIN_APP, GOOGLE_LOGIN_CLIENT_SIG);
+			return new TokenInfo(authToken, refreshToken);
+		} catch (IOException e) {
+			throw new RemoteServerException(e);
+		} catch (Gpsoauth.TokenRequestFailed e) {
+			throw new LoginFailedException(e);
+		}
+	}
 
 	@Override
 	public String getTokenId() throws LoginFailedException, RemoteServerException {
-		if (isTokenIdExpired()) login(username, password);
-		return authToken.getToken();
+		if (isTokenIdExpired()) {
+			this.tokenInfo = refreshToken(username, tokenInfo.refreshToken);
+		}
+		return tokenInfo.authToken.getToken();
 	}
 
 	@Override
 	public AuthInfo getAuthInfo() throws LoginFailedException, RemoteServerException {
 		AuthInfo.Builder builder = AuthInfo.newBuilder();
 		builder.setProvider("google");
-		builder.setToken(AuthInfo.JWT.newBuilder().setContents(authToken.getToken()).setUnknown2(59).build());
+		builder.setToken(AuthInfo.JWT.newBuilder().setContents(tokenInfo.authToken.getToken()).setUnknown2(59).build());
 		return builder.build();
 	}
 
 	@Override
 	public boolean isTokenIdExpired() {
-		return authToken.getExpiry() > System.currentTimeMillis() / 1000;
+		return tokenInfo.authToken.getExpiry() > System.currentTimeMillis() / 1000 - 60;
+	}
+
+	private static class TokenInfo {
+		final AuthToken authToken;
+		final String refreshToken;
+
+		TokenInfo(AuthToken authToken, String refreshToken) {
+			this.authToken = authToken;
+			this.refreshToken = refreshToken;
+		}
 	}
 }
