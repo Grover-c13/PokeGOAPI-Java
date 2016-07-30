e0d35c3f9d0f09bd138b39732167189d9b1327f3
Paul van Assen <source@pvanassen.nl>
2016-07-30 18:04:39 +0200

    Checkstyle update

--- src/main/java/com/pokegoapi/util/NestedFutureWrapper.java
+++ src/main/java/com/pokegoapi/util/NestedFutureWrapper.java
@@ -7,34 +7,34 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
-public abstract class NestedFutureWrapper<T,R> extends FutureWrapper<T,R> {
-    public NestedFutureWrapper(Future<T> result) {
-        super(result);
-    }
+public abstract class NestedFutureWrapper<T, R> extends FutureWrapper<T, R> {
+	public NestedFutureWrapper(Future<T> result) {
+		super(result);
+	}
 
-    protected R getResult(long timeouut, TimeUnit timeUnit) throws InterruptedException, ExecutionException {
-        long wait = System.currentTimeMillis() + timeUnit.toMillis(timeouut);
-        while (!isDone()) {
-            Thread.sleep(10);
-            if (wait < System.currentTimeMillis()) {
-                return null;
-            }
-        }
-        Future<R> future = handleFuture(result.get());
-        while (future.isDone()) {
-            Thread.sleep(10);
-            if (wait < System.currentTimeMillis()) {
-                return null;
-            }
-        }
-        return future.get();
-    }
+	protected R getResult(long timeouut, TimeUnit timeUnit) throws InterruptedException, ExecutionException {
+		long wait = System.currentTimeMillis() + timeUnit.toMillis(timeouut);
+		while (!isDone()) {
+			Thread.sleep(10);
+			if (wait < System.currentTimeMillis()) {
+				return null;
+			}
+		}
+		Future<R> future = handleFuture(result.get());
+		while (future.isDone()) {
+			Thread.sleep(10);
+			if (wait < System.currentTimeMillis()) {
+				return null;
+			}
+		}
+		return future.get();
+	}
 
-    protected abstract Future<R> handleFuture(T result);
+	protected abstract Future<R> handleFuture(T result);
 
-    @Override
-    protected final R handle(T result) throws RemoteServerException, LoginFailedException {
-    	// Because getResult is overiden, this is not called
-        return null;
-    }
+	@Override
+	protected final R handle(T result) throws RemoteServerException, LoginFailedException {
+		// Because getResult is overiden, this is not called
+		return null;
+	}
 }
