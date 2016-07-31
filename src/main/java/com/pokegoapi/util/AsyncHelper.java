f38e834190132084f9a8ee8ad664ea31caadc6af
Paul van Assen <pvanassen@synedge.com>
2016-07-31 13:16:36 +0200

    Updated asyncHelper because of checkstyle issues

--- src/main/java/com/pokegoapi/util/AsyncHelper.java
+++ src/main/java/com/pokegoapi/util/AsyncHelper.java
@@ -1,3 +1,17 @@
+/*
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.pokegoapi.util;
 
 import com.pokegoapi.exceptions.AsyncLoginFailedException;
@@ -7,22 +21,27 @@ import com.pokegoapi.exceptions.LoginFailedException;
 import com.pokegoapi.exceptions.RemoteServerException;
 import rx.Observable;
 
-/**
- * Created by paul on 30-7-2016.
- */
 public class AsyncHelper {
-    public static <T> T toBlocking(Observable<T> observable) throws LoginFailedException, RemoteServerException {
-        try {
-            return observable.toBlocking().first();
-        }
-        catch (RuntimeException e) {
-            if (e.getCause() instanceof AsyncLoginFailedException) {
-                throw new LoginFailedException(e.getMessage(), e.getCause());
-            }
-            if (e.getCause() instanceof AsyncRemoteServerException) {
-                throw new RemoteServerException(e.getMessage(), e.getCause());
-            }
-            throw new AsyncPokemonGoException("Unknown exception occurred. ", e);
-        }
-    }
+	/**
+	 * Convert an observable to the actual result, recovering the actual exception and throwing that
+	 *
+	 * @param observable Observable to handle
+	 * @param <T>        Result type
+	 * @return Result of the observable
+	 * @throws LoginFailedException  If an AsyncLoginFailedException was thrown
+	 * @throws RemoteServerException If an AsyncRemoteServerException was thrown
+	 */
+	public static <T> T toBlocking(Observable<T> observable) throws LoginFailedException, RemoteServerException {
+		try {
+			return observable.toBlocking().first();
+		} catch (RuntimeException e) {
+			if (e.getCause() instanceof AsyncLoginFailedException) {
+				throw new LoginFailedException(e.getMessage(), e.getCause());
+			}
+			if (e.getCause() instanceof AsyncRemoteServerException) {
+				throw new RemoteServerException(e.getMessage(), e.getCause());
+			}
+			throw new AsyncPokemonGoException("Unknown exception occurred. ", e);
+		}
+	}
 }
