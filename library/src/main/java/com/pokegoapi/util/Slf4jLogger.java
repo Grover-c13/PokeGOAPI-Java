25c01db3abb01395176648ae8351dfa405a3e189
Paul van Assen <pvanassen@synedge.com>
2016-08-11 08:37:22 +0200

    Better logger

--- library/src/main/java/com/pokegoapi/util/Slf4jLogger.java
+++ library/src/main/java/com/pokegoapi/util/Slf4jLogger.java
@@ -1,83 +1,82 @@
 package com.pokegoapi.util;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Paul van Assen
  */
-@Slf4j
 public class Slf4jLogger implements Logger {
     @Override
     public void v(String tag, String msg) {
-        log.debug(getMessage(tag, msg));
+        getLogger(tag).debug(msg);
     }
 
     @Override
     public void v(String tag, String msg, Throwable tr) {
-        log.debug(getMessage(tag, msg), tr);
+        getLogger(tag).debug(msg, tr);
     }
 
     @Override
     public void d(String tag, String msg) {
-        log.debug(getMessage(tag, msg));
+        getLogger(tag).debug(msg);
     }
 
     @Override
     public void d(String tag, String msg, Throwable tr) {
-        log.debug(getMessage(tag, msg), tr);
+        getLogger(tag).debug(msg, tr);
     }
 
     @Override
     public void i(String tag, String msg) {
-        log.info(getMessage(tag, msg));
+        getLogger(tag).info(msg);
     }
 
     @Override
     public void i(String tag, String msg, Throwable tr) {
-        log.info(getMessage(tag, msg), tr);
+        getLogger(tag).info(msg, tr);
     }
 
     @Override
     public void w(String tag, String msg) {
-        log.warn(getMessage(tag, msg));
+        getLogger(tag).warn(msg);
     }
 
     @Override
     public void w(String tag, String msg, Throwable tr) {
-        log.warn(getMessage(tag, msg), tr);
+        getLogger(tag).warn(msg, tr);
     }
 
     @Override
     public void w(String tag, Throwable tr) {
-        log.warn(tag, tr);
+        getLogger(tag).warn(tag, tr);
     }
 
     @Override
     public void e(String tag, String msg) {
-        log.error(getMessage(tag, msg));
+        getLogger(tag).error(msg);
     }
 
     @Override
     public void e(String tag, String msg, Throwable tr) {
-        log.error(getMessage(tag, msg), tr);
+        getLogger(tag).error(msg, tr);
     }
 
     @Override
     public void wtf(String tag, String msg) {
-        log.error(getMessage(tag, msg));
+        getLogger(tag).error(msg);
     }
 
     @Override
     public void wtf(String tag, Throwable tr) {
-        log.error(tag, tr);
+        getLogger(tag).error(tag, tr);
     }
 
     @Override
     public void wtf(String tag, String msg, Throwable tr) {
-        log.error(getMessage(tag, msg), tr);
+        getLogger(tag).error(msg, tr);
     }
 
-    private String getMessage(String tag, String msg) {
-        return tag.concat(" - ").concat(msg);
+    private org.slf4j.Logger getLogger(String tag) {
+        return LoggerFactory.getLogger(tag);
     }
 }
