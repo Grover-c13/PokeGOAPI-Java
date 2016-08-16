b8188bbd6a95dbd7c8a522bd30541788787c2b49
gionata-bisciari <gionatabisciari@gmail.com>
2016-08-16 19:34:16 +0200

    Added new useful methods to Pokemon.java
    +getStaminaInPercentage():int
    +getCPInPercentageActualPlayerLevel():int
    +getCPInPercentageMaxPlayerLevel():int
    +getIvInPercentage():double

--- library/src/main/java/com/pokegoapi/api/pokemon/Pokemon.java
+++ library/src/main/java/com/pokegoapi/api/pokemon/Pokemon.java
@@ -23,6 +23,7 @@ import com.pokegoapi.api.map.pokemon.EvolutionResult;
 import com.pokegoapi.api.player.PlayerProfile;
 import com.pokegoapi.exceptions.AsyncRemoteServerException;
 import com.pokegoapi.exceptions.LoginFailedException;
+import com.pokegoapi.exceptions.NoSuchItemException;
 import com.pokegoapi.exceptions.RemoteServerException;
 import com.pokegoapi.main.AsyncServerRequest;
 import com.pokegoapi.main.ServerRequest;
@@ -402,5 +403,37 @@ public class Pokemon extends PokemonDetails {
 
 	public EvolutionForm getEvolutionForm() {
 		return new EvolutionForm(getPokemonId());
+	}
+	
+	/**
+	 * @return Actual stamina in percentage relative to the current maximum stamina (useful in ProgressBars)
+	 */
+	public int getStaminaInPercentage() {
+		return (getStamina()*100)/getMaxStamina();
+	}
+	
+	/**
+	 * @return Actual cp in percentage relative to the maximum cp that this pokemon can reach at the actual player level (useful in ProgressBars)
+	 * @throws NoSuchItemException if threw from {@link #getMaxCpForPlayer()}
+	 * @throws LoginFailedException if threw from {@link #getMaxCpForPlayer()}
+	 * @throws RemoteServerException if threw from {@link #getMaxCpForPlayer()}
+	 */
+	public int getCPInPercentageActualPlayerLevel() throws NoSuchItemException, LoginFailedException, RemoteServerException{
+		return ((getCp()*100)/getMaxCpForPlayer());
+	}
+	
+	/**
+	 * @return Actual cp in percentage relative to the maximum cp that this pokemon can reach at player-level 40 (useful in ProgressBars)
+	 * @throws NoSuchItemException if threw from {@link #getMaxCp()}
+	 */
+	public int getCPInPercentageMaxPlayerLevel() throws NoSuchItemException{
+		return ((getCp()*100)/getMaxCp());
+	}
+	
+	/**
+	 * @return IV in percentage
+	 */
+	public double getIvInPercentage(){
+		return ((Math.floor((this.getIvRatio()*100)*100))/100);
 	}
 }
