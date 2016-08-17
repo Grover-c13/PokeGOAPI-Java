ba8f6eb27b600d2a339d0d43db5a057e7679c7d2
Nyazuki <neko.thfp@gmail.com>
2016-08-17 17:13:46 -0400

    Update examples for DisplayPokename util

--- sample/src/main/java/com/pokegoapi/examples/DisplayPokenameExample.java
+++ sample/src/main/java/com/pokegoapi/examples/DisplayPokenameExample.java
@@ -17,8 +17,9 @@ package com.pokegoapi.examples;
 
 import com.pokegoapi.util.Log;
 import com.pokegoapi.util.PokeNames;
-
+import java.io.UnsupportedEncodingException;
 import java.util.Locale;
+import java.util.MissingResourceException;
 
 public class DisplayPokenameExample {
 
@@ -41,32 +42,51 @@ public class DisplayPokenameExample {
 				new Locale("zh", "HK"),
 				new Locale("zh", "TW"),
 		};
+
 		for (int i = 1; i < 152; i++) {
 			//Showcase for Supported Languages
 			for (Locale l : supportedLocales) {
-				Log.d("Names-Example", String.format(
-						l,
-						"Pokedex Nr# %d is %s in %s",
-						i,
-						PokeNames.getDisplayName(i, l),
-						l.getDisplayName(l)));
+				try {
+					Log.d("Names-Example", String.format(
+							l,
+							"Pokedex# %d is %s in %s",
+							i,
+							PokeNames.getDisplayName(i, l),
+							l.getDisplayName(l)));
+				} catch (UnsupportedEncodingException e) {
+					Log.e("Main", "Unable to display name with given locale: ", e);
+				} catch (MissingResourceException e) {
+					Log.e("Main", "Unable to find Pokemon name with given Pokedex: ", e);
+				}
 			}
-			//Showcase for Fallback Behaviour
-			Log.d("Names-Example", String.format(
-					"Pokedex Nr# %d is %s in %s",
-					i,
-					PokeNames.getDisplayName(i, new Locale("xx")), "Fallback"));
+			//Showcase for Fallback Behaviourn
+			try {
+				Log.d("Names-Example", String.format(
+						"Pokedex# %d is %s in %s",
+						i,
+						PokeNames.getDisplayName(i, new Locale("xx")), "Fallback"));
+			} catch (UnsupportedEncodingException e) {
+				Log.e("Main", "Unable to display name with given locale: ", e);
+			} catch (MissingResourceException e) {
+				Log.e("Main", "Unable to find Pokemon name with given Pokedex: ", e);
+			}
 		}
 
 		for (int i = 1; i < 152; i++) {
 			//Translate English Pokemon name to Simplified Chinese
 			Locale chs = new Locale("zh", "CN");
-			Log.d("English Names to Simplified Chinese - Example", String.format(
-					chs,
-					"Pokedex Nr# %d is %s in %s",
-					i,
-					PokeNames.translateName(PokeNames.getDisplayName(i, Locale.ENGLISH), chs),
-					chs.getDisplayName(chs)));
+			try {
+				Log.d("Translate English Names to Simplified Chinese - Example", String.format(
+						chs,
+						"Pokedex# %d is %s in %s",
+						i,
+						PokeNames.translateName(PokeNames.getDisplayName(i, Locale.ENGLISH), chs),
+						chs.getDisplayName(chs)));
+			} catch (UnsupportedEncodingException e) {
+				Log.e("Main", "Unable to display name with given locale: ", e);
+			} catch (MissingResourceException e) {
+				Log.e("Main", "Unable to find Pokemon name with given Pokedex: ", e);
+			}
 		}
 	}
 }
