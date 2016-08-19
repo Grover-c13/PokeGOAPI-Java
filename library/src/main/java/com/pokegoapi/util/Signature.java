f169c3aa861a5d9e3c235763e0b5758c25c25827
iGio90 <giovanni.rocca.90@gmail.com>
2016-08-19 22:25:37 +0200

    Revert to use ios device config but prevent empty infos

--- library/src/main/java/com/pokegoapi/util/Signature.java
+++ library/src/main/java/com/pokegoapi/util/Signature.java
@@ -8,19 +8,18 @@ import POGOProtos.Networking.Requests.RequestOuterClass;
 
 import com.google.protobuf.ByteString;
 import com.pokegoapi.api.PokemonGo;
+import com.pokegoapi.api.device.DeviceInfo;
 import com.pokegoapi.util.Crypto;
-import com.pokegoapi.util.Log;
 
 import net.jpountz.xxhash.StreamingXXHash32;
 import net.jpountz.xxhash.StreamingXXHash64;
 import net.jpountz.xxhash.XXHashFactory;
 
-import java.lang.reflect.Method;
 import java.util.Random;
 
 public class Signature {
 
-	private static SignatureOuterClass.Signature.DeviceInfo sDeviceInfo;
+	private static DeviceInfo sDeviceInfo;
 
 	/**
 	 * Given a fully built request, set the signature correctly.
@@ -45,19 +44,16 @@ public class Signature {
 				.setTimestamp(api.currentTimeMillis())
 				.setTimestampSinceStart(curTime - api.startTime);
 
-		if (sDeviceInfo == null) {
+		if (sDeviceInfo != null) {
+			sigBuilder.setDeviceInfo(sDeviceInfo.getDeviceInfo());
+		} else {
 			if (api.getDeviceInfo() != null) {
-				sDeviceInfo = api.getDeviceInfo();
-			}
-
-			// fallback
-			if (sDeviceInfo == null) {
-				sDeviceInfo = buildDeviceInfo();
+				sigBuilder.setDeviceInfo(api.getDeviceInfo());
+			} else {
+				sDeviceInfo = new DeviceInfo();
+				sigBuilder.setDeviceInfo(sDeviceInfo.getDeviceInfo());
 			}
 		}
-
-		Log.e("DeviceInfo", sDeviceInfo.toString());
-		sigBuilder.setDeviceInfo(sDeviceInfo);
 
 		SignatureOuterClass.Signature.SensorInfo sensorInfo = api.getSensorInfo();
 		if (sensorInfo != null) {
@@ -132,116 +128,5 @@ public class Signature {
 		xx64 = factory.newStreamingHash64(xx64.getValue());
 		xx64.update(request, 0, request.length);
 		return xx64.getValue();
-	}
-
-	private static SignatureOuterClass.Signature.DeviceInfo buildDeviceInfo() {
-		String customId = getUniqueId();
-		if (customId.isEmpty()) {
-			return SignatureOuterClass.Signature.DeviceInfo.newBuilder().build();
-		}
-
-		return SignatureOuterClass.Signature.DeviceInfo.newBuilder()
-				.setDeviceId(customId)
-				.setAndroidBoardName("shamu")
-				.setAndroidBootloader("moto-apq8084-71.15")
-				.setDeviceBrand("google")
-				.setDeviceModel("shamu")
-				.setDeviceModelIdentifier("MRA58R")
-				.setDeviceModelBoot("shamu")
-				.setHardwareManufacturer("motorola")
-				.setHardwareModel("Nexus 6")
-				.setFirmwareBrand("shamu")
-				.setFirmwareTags("release-keys")
-				.setFirmwareType("user")
-				.setFirmwareFingerprint("google/shamu/shamu:6.0/MRA58R/2308909:user/release-keys")
-				.build();
-	}
-
-	/**
-	 * @TODO: Something more or something cleaner?
-	 *
-	 * @return an unique id for this device
-	 */
-	private static String getUniqueId() {
-		try {
-			String serial = getDeviceId();
-			if (serial.isEmpty()) {
-				return serial;
-			}
-			String[] splittedSerial = serial.split("");
-			serial = "";
-			int i = 0;
-			int k = 0;
-			if (serial.length() == 16) {
-				// good to go
-			}  else if (serial.length() > 16) {
-				serial = serial.substring(0, 16);
-			} else {
-				while (serial.length() != 16) {
-					try {
-						String s = splittedSerial[i];
-						if (isNumber(s)) {
-							s = fakeNumber(s);
-						}
-
-						serial = serial + s;
-						i++;
-					} catch (IndexOutOfBoundsException e) {
-						i = k;
-						k = k == 1 ? 0 : 1;
-					}
-				}
-			}
-			return serial.toLowerCase();
-		} catch (Exception exception) {
-			return "";
-		}
-	}
-
-	private static String getDeviceId() {
-		try {
-			Class<?> c = Class.forName("android.os.SystemProperties");
-			Method get = c.getMethod("get", String.class, String.class);
-			return (String) (get.invoke(c, "ro.serialno", "unknown"));
-		} catch (Exception ignored) {
-			return "";
-		}
-	}
-
-	private static String fakeNumber(String s) {
-		int i = Integer.valueOf(s);
-		switch (i) {
-			case 0:
-				return String.valueOf(3);
-			case 1:
-				return String.valueOf(7);
-			case 2:
-				return String.valueOf(0);
-			case 3:
-				return String.valueOf(9);
-			case 4:
-				return String.valueOf(1);
-			case 5:
-				return String.valueOf(2);
-			case 6:
-				return String.valueOf(8);
-			case 7:
-				return String.valueOf(4);
-			case 8:
-				return String.valueOf(5);
-			case 9:
-				return String.valueOf(6);
-			default:
-				return String.valueOf(0);
-		}
-	}
-
-	private static boolean isNumber(String str) {
-		try {
-			Double d = Double.parseDouble(str);
-			return true;
-		} catch(NumberFormatException nfe) {
-			return false;
-		}
 	}
 }
