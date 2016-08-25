86b169917be6e1a0f9877dde4e9f700fc45c9e29
fabianterhorst <fabian.terhorst@gmail.com>
2016-08-25 10:22:25 +0200

    cleanup imports

--- library/src/main/java/com/pokegoapi/api/device/LocationFixes.java
+++ library/src/main/java/com/pokegoapi/api/device/LocationFixes.java
@@ -4,7 +4,6 @@ import com.pokegoapi.api.PokemonGo;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Random;
 
 import POGOProtos.Networking.Envelopes.RequestEnvelopeOuterClass;
