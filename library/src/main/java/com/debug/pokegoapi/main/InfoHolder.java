4084be987c54f89e669f8dcc6a6b5a2a016dbfd5
Paul van Assen <pvanassen@synedge.com>
2016-08-29 09:24:54 +0200

    Integrated debug

---
+++ library/src/main/java/com/debug/pokegoapi/main/InfoHolder.java
@@ -0,0 +1,25 @@
+package com.debug.pokegoapi.main;
+
+import POGOProtos.Networking.Envelopes.RequestEnvelopeOuterClass.RequestEnvelope.AuthInfo;
+import POGOProtos.Networking.Envelopes.SignatureOuterClass.Signature.DeviceInfo;
+import com.debug.pokegoapi.api.device.ActivityStatus;
+import com.debug.pokegoapi.api.device.LocationFixes;
+import com.debug.pokegoapi.api.device.SensorInfo;
+import lombok.Data;
+
+/**
+ * @author Paul van Assen
+ */
+@Data
+public class InfoHolder {
+	private final long startTime = System.currentTimeMillis();
+	private double latitude;
+	private double longitude;
+	private double altitude;
+	private AuthInfo authInfo;
+	private byte[] sessionHash;
+	private DeviceInfo deviceInfo;
+	private ActivityStatus activityStatus;
+	private LocationFixes locationFixes;
+	private SensorInfo sensorInfo;
+}
