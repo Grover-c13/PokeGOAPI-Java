38c637157f57d24f8d6efc3caed6c2b8373f2a15
rama <rama.rama@tiscali.it>
2016-08-31 21:30:25 +0200

    test wait function on callback (will be removed!! debug purpose only)

--- library/src/main/java/com/pokegoapi/util/PokeCallback.java
+++ library/src/main/java/com/pokegoapi/util/PokeCallback.java
@@ -3,19 +3,24 @@ package com.pokegoapi.util;
 import com.pokegoapi.api.map.MapObjects;
 import rx.Subscriber;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Created by rama on 31/08/16.
  */
 public abstract class PokeCallback<T> {
+
+	private AtomicLong barrier = new AtomicLong(0);
 
 	public abstract void onError(Throwable e);
 
 	public abstract void onResponse(T data);
 
 	public Subscriber<T> getSubscriber() {
+		barrier.incrementAndGet();
 		return new Subscriber<T>() {
 			public void onCompleted() {
-
+				barrier.set(0);
 			}
 
 			@Override
@@ -28,5 +33,21 @@ public abstract class PokeCallback<T> {
 				onResponse(mapObjects);
 			}
 		};
+	}
+
+	public boolean completed() {
+		return barrier.compareAndSet(0, 0);
+	}
+
+	public void waitComplete() {
+		while (true) {
+			if (barrier.compareAndSet(0, 0))
+				return;
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+
+			}
+		}
 	}
 }
